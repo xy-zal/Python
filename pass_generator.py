3fe95edee90112721c416bd76fb9a8d65ceec74e
Rizal Wahyu <xy-zal@users.noreply.github.com>
2020-12-08 13:01:10 +0700

    first commit

---
+++ pass_generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+LETTERS = string.ascii_letters
+NUMS = '0123456789'
+SPE = '!@#%&*'
+FINAL = LETTERS + NUMS + SPE
+
+len = int(input("PANJANG PASSWORD: "))
+password = "".join(random.sample(FINAL, len))
+print(password)
